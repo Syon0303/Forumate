9607c5a5e2d0e0777e5358e9f65efc77b7120293
bepyan <bepyan@naver.com>
2020-05-18 19:24:21 +0900

    구현1차

---
+++ Forumate/src/forumate/controller/User_main.java
@@ -0,0 +1,25 @@
+package forumate.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import forumate.app.App;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class User_main implements Initializable{
+	@FXML private AnchorPane myGroup;
+	@FXML private AnchorPane group;
+	@FXML private AnchorPane home;
+	@FXML private AnchorPane calendar;
+	@FXML private AnchorPane facility;
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		myGroup.setOnMouseClicked(e -> App.go("user_myGroup.fxml"));
+		group.setOnMouseClicked(e -> App.go("user_group.fxml"));
+		home.setOnMouseClicked(e -> App.go("user_main.fxml"));
+		calendar.setOnMouseClicked(e -> App.go("user_calendar.fxml"));
+		facility.setOnMouseClicked(e -> App.go("user_facility.fxml"));
+	}
+}
